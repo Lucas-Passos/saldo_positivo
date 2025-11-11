cc239aaaed9bd4eee5acde1ba4005eac2f05b355
Lucas-Passos <luksspassos@gmail.com>
2025-11-11 16:04:33 -0300

    feat: Refactor ResultadoScreen to use StatefulWidget and implement saldo calculation with Hive data

--- lib/screens/resultado_screen.dart
+++ lib/screens/resultado_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../datasource/hive_datasource.dart';
+import '../models/receita.dart';
+import '../models/despesa.dart';
 import '../widgets/saldo_card.dart';
 
-class ResultadoScreen extends StatelessWidget {
+class ResultadoScreen extends StatefulWidget {
   const ResultadoScreen({super.key});
 
   @override
+  State<ResultadoScreen> createState() => _ResultadoScreenState();
+}
+
+class _ResultadoScreenState extends State<ResultadoScreen> {
+  final HiveDataSource _data = HiveDataSource();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: SaldoCard(saldo: 0.0)), // saldo calculado futuramente
+    final receitaBox = Hive.box<Receita>('receitas');
+    final despesaBox = Hive.box<Despesa>('despesas');
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Saldo Atual')),
+      body: ValueListenableBuilder(
+        valueListenable: receitaBox.listenable(),
+        builder: (context, Box<Receita> receitas, _) {
+          return ValueListenableBuilder(
+            valueListenable: despesaBox.listenable(),
+            builder: (context, Box<Despesa> despesas, _) {
+              final saldo = _data.calcularSaldo();
+              return Center(child: SaldoCard(saldo: saldo));
+            },
+          );
+        },
+      ),
     );
   }
 }
